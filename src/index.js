e2df171a3d47180e792410cbbc7312843cab89a8
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-10-14 01:17:41 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
-import { fetch} from './fetch';
+import { fetchCountries} from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
